20e560b8afd2fb81f47280f20fd05fbc56e4026d
Daniel Clapp <danielclapp94@gmail.com>
2023-07-26 21:01:17 -0700

    got the background color button working

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const button = document.querySelector('#color');
 
+function changeColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  const newColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.backgroundColor = newColor;
+}
+
+button.addEventListener('click', changeColor);
